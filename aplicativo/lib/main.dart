eb5234af8e1be0e0352f81d99feaa5796efa6292
henriquefugie <henriquefugie@gmail.com>
2023-03-21 18:54:19 -0300

    atualizado

--- aplicativo/lib/main.dart
+++ aplicativo/lib/main.dart
@@ -37,223 +37,228 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Widget buildImagePreview(BuildContext context, int index) {
+    return GestureDetector(
+      onTap: () {
+        // implement a full-screen preview of the image
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.file(
+            _imageList[index],
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Visita Técnica'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _homeKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Cliente:'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter a value';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Morada:'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter a value';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(labelText: 'Data da Visita:'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter a value';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  const Text('Rede',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          _switchValue ? 'Trifásica' : 'Monofásica',
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Toque no switch para alternar entre Monofásica e Trifásica.',
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Visita Técnica'),
+      ),
+      body: Expanded(
+        child: Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _homeKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Cliente:'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Morada:'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Data da Visita:'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    const Text('Rede',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            _switchValue ? 'Trifásica' : 'Monofásica',
+                            textAlign: TextAlign.left,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Switch(
-                    value: _switchValue,
-                    onChanged: (value) {
-                      setState(() {
-                        _switchValue = value;
-                      });
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        labelText: 'Potência contratada:'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter a value';
-                      }
-                      return null;
-                    },
-                  ),
-                  const Text('Facturas'),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: GridView.count(
-                          crossAxisCount: 3,
-                          children: _imageList.map((image) {
-                            return Image.file(image);
-                          }).toList(),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: SizedBox(
-                          height: 20,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: _imageList.length,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.file(
-                                  _imageList[index],
-                                  width: 100,
-                                ),
-                              );
-                            },
+                        Expanded(
+                          child: Text(
+                            'Toque no switch para alternar entre Monofásica e Trifásica.',
+                            style: TextStyle(
+                              color: Colors.black.withOpacity(0.5),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  const Text('Baterias',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          _switchValue1 ? 'Sim' : 'Não',
-                          textAlign: TextAlign.left,
+                      ],
+                    ),
+                    Switch(
+                      value: _switchValue,
+                      onChanged: (value) {
+                        setState(() {
+                          _switchValue = value;
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: 'Potência contratada:'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    const Text('Facturas'),
+                    SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _imageList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return buildImagePreview(context, index);
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _addImages,
+                child: Text('Add Image'),
+              ),
+              
+                    const SizedBox(height: 20),
+                    const Text('Baterias',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            _switchValue1 ? 'Sim' : 'Não',
+                            textAlign: TextAlign.left,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Switch(
-                    value: _switchValue1,
-                    onChanged: (value) {
-                      setState(() {
-                        _switchValue1 = value;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  const Text('Cobertura',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          _switchValue2 ? 'Inclinada' : 'Plana',
-                          textAlign: TextAlign.left,
+                      ],
+                    ),
+                    Switch(
+                      value: _switchValue1,
+                      onChanged: (value) {
+                        setState(() {
+                          _switchValue1 = value;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    const Text('Cobertura',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            _switchValue2 ? 'Inclinada' : 'Plana',
+                            textAlign: TextAlign.left,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Switch(
-                    value: _switchValue2,
-                    onChanged: (value) {
-                      setState(() {
-                        _switchValue2 = value;
-                      });
-                    },
-                  ),
-                  const Text('Caso a cobertura seja inclinada.',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        labelText: 'Pé Direito da parte mais alta:'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter a value';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        labelText: 'Pé Direito da parte mais baixa:'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter a value';
-                      }
-                      return null;
-                    },
-                  ),
-                  const Text('Distâncias',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Pé Direito:'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter a value';
-                      }
-                      return null;
-                    },
-                  ),
-                  const Text('Quadros e passagens de cabos',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  const Text('Fotos Adicionais',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  const Text('Informações Adicionais',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_homeKey.currentState!.validate()) {
-                        // TODO: Submit form
-                      }
-                    },
-                    child: const Text('Salvar'),
-                  ),
-                ],
+                      ],
+                    ),
+                    Switch(
+                      value: _switchValue2,
+                      onChanged: (value) {
+                        setState(() {
+                          _switchValue2 = value;
+                        });
+                      },
+                    ),
+                    const Text('Caso a cobertura seja inclinada.',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: 'Pé Direito da parte mais alta:'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: 'Pé Direito da parte mais baixa:'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    const Text('Distâncias',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Pé Direito:'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    const Text('Quadros e passagens de cabos',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    const Text('Fotos Adicionais',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    const Text('Informações Adicionais',
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_homeKey.currentState!.validate()) {
+                          // TODO: Submit form
+                        }
+                      },
+                      child: const Text('Salvar'),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
